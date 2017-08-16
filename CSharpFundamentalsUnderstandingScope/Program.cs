a805cca3c6302c6571c964a020a1bf3d1ec5141e
Burris, Wayne <Wayne.Burris@protective.com>
2017-08-16 15:00:21 -0500

    added for loop to log 1-9

--- CSharpFundamentalsUnderstandingScope/Program.cs
+++ CSharpFundamentalsUnderstandingScope/Program.cs
@@ -10,7 +10,11 @@ namespace CSharpFundamentalsUnderstandingScope
     {
         static void Main(string[] args)
         {
-
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
         }
     }
 }
