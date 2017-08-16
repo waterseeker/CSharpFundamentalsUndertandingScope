3894e6683b8c836839296ae0e05c57fe957f67d8
Burris, Wayne <Wayne.Burris@protective.com>
2017-08-16 15:04:09 -0500

    added example of writing an outside var from inside the for loop

--- CSharpFundamentalsUnderstandingScope/Program.cs
+++ CSharpFundamentalsUnderstandingScope/Program.cs
@@ -10,12 +10,18 @@ namespace CSharpFundamentalsUnderstandingScope
     {
         static void Main(string[] args)
         {
+            string j = "";
+
             for (int i = 0; i < 10; i++)
             {
+                j = i.ToString();
                 Console.WriteLine(i);
             }
             //you can't access i because it does not exist outside of its code block
             //Console.WriteLine(i); 
+
+            Console.WriteLine("Outside of the for: " + j);
+
             Console.ReadLine();
         }
     }
